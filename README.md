34c4ecf4e0829ace062a31564e75d70f793650e3
Ali <ali918273645@gmail.com>
2018-07-24 15:51:17 -0500

    Adding sounds and color

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # simonsays
+
+v0.2.0
+This is a program that play sounds and colors in the order of a memory game.
--- simonsays.py
+++ simonsays.py
@@ -3,7 +3,16 @@ import LEDRGB as LED
 import time
 import random
 
-colors = ['R', 'G', 'B', 'Y']
+colors = ['R','Y','G','B']
+sounds = [262,392,591,880]
+list_color = [ ]
+list_sound = [ ]
+
+GPIO.setmode(GPIO.BOARD)
+buzz_pin = 32
+
+GPIO.setup(buzz_pin,GPIO.OUT)
+Buzz = GPIO.PWM(buzz_pin,1000)
 
 R_pin = 11
 G_pin = 12
@@ -11,18 +20,31 @@ B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
 GPIO.setwarnings(False)
 
-def rand_color():
-    while True:
-        n = random.randint(0,3)
-        LED.setColor(colors[n])
-        time.sleep(0.5)
+def play_list(listc,lists):
+    for x in range(0,len(listc)-1):
+        LED.setColor(listc[x])
+        Buzz.ChangeFrequency(lists[x])
+        Buzz.start(50)
+        time.sleep(0.25)
+        
+        LED.noColor()
+        Buzz.stop()
+        time.sleep(0.25)
+
+def append_list():
+    n = random.randint(0,3)
+    list_color.append(colors[n])
+    list_sound.append(sounds[n])
 
 def turn_off():
     LED.destroy()
 
 if __name__ == '__main__':
     try:
-        rand_color()
+        while True:       
+            append_list()
+            play_list(list_color,list_sound)
+            time.sleep(0.75)
     except KeyboardInterrupt:
         GPIO.setwarnings(False)        
         turn_off()
